449312ee94f54af808645fd95001ddbe64c77d20
stark world <nicky.kalyan438@gmail.com>
2020-08-19 18:20:32 -0400

    Player Puuid same

--- server.js
+++ server.js
@@ -92,9 +92,18 @@ app.get("/val/match/v1/matchlists/by-puuid/:puuid", (req, res) => {
   const reqPuuid = req.params.puuid;
   const dbRes = db.get("summoners").find({ puuid: reqPuuid }).value();
   if (typeof dbRes !== "undefined") {
+    let matchHistoryList = []
+    for (let match of dbRes.matchList) {
+      let matchObj = {
+        matchId: match,
+        gameStartTimeMillis: ,
+        teamID: RED OR BLUE
+      }
+      matchHistoryList.push(matchObj);
+    }
     const response = {
       puuid: dbRes.puuid,
-      history: dbRes.matchList,
+      history: matchHistoryList,
     };
     res.json(response);
   } else {
@@ -124,58 +133,59 @@ app.get("/", (req, res) => {
 
 app.get("/status", statusMonitor.pageRoute);
 
-// app.get("/init", (req, res) => {
-//   db.defaults({ summoners: [], matches: [] }).write();
-//   res.send("Successfully initialized DB!");
-//  });
+app.get("/init", (req, res) => {
+  db.defaults({ summoners: [], matches: [] }).write();
+  res.send("Successfully initialized DB!");
+ });
 
-//  app.get("/generate", (req, res) => {
-//   let matchIds = [];
-//   for (var i = 0; i < n; i++) {
-//     var summoner = {};
-//     summoner.puuid = casual.uuid;
-//     summoner.gameName = casual.username;
-//     summoner.tagLine = casual.integer((from = 1), (to = 1000));
-//     summoner.activeShard =
-//       activeShards[Math.floor(Math.random() * activeShards.length)];
-//     summoner.game = "val";
-//     summoner.matchList = new Array(2).fill().map(() => casual.uuid);
-//     matchIds.push(summoner.matchList);
-//     db.get("summoners").push(summoner).write();
-//     console.log("summoner saved: ", summoner.puuid);
-//   }
-//   matchIds = [].concat.apply([], matchIds);
-//   for (let matchId of matchIds) {
-//     console.log("For : ", matchId);
-//     let matchObj = {};
-//     //matchInfo
-//     matchObj.matchInfo = [utils.generateMatchInfoDTO(matchId)];
-//     //players
-//     let playerList = [];
-//     for (var x = 0; x < 10; x++) {
-//       playerList.push(utils.generatePlayerDTO());
-//     }
-//     matchObj.players = playerList;
-//     //teams
-//     let teams = [];
-//     let arr = ["Red", "Blue"];
-//     teamBool = casual.boolean;
-//     for (var t = 0; t < 2; t++) {
-//       teams.push(utils.generateTeamDTO(arr[t], teamBool));
-//     }
-//     matchObj.teams = teams;
-//     //roundResults
-//     matchObj.roundResults = [];
-//     for (var r = 0; r < 12; r++) {
-//       matchObj.roundResults.push(utils.generateRoundResultDTO(r));
-//     }
-//     //Push to DB
-//     db.get("matches").push({ id: matchId, data: matchObj }).write();
-//   }
-//   res.send(
-//     `Successfully generated ${n} summoners and ${matchIds.length} matches!`
-//   );
-// });
+ app.get("/generate", (req, res) => {
+  let matchIds = [];
+  for (var i = 0; i < n; i++) {
+    var summoner = {};
+    summoner.puuid = casual.uuid;
+    summoner.gameName = casual.username;
+    summoner.tagLine = casual.integer((from = 1), (to = 1000));
+    summoner.activeShard =
+      activeShards[Math.floor(Math.random() * activeShards.length)];
+    summoner.game = "val";
+    summoner.matchList = new Array(2).fill().map(() => casual.uuid);
+    matchIds.push(summoner.matchList);
+    db.get("summoners").push(summoner).write();
+    console.log("summoner saved: ", summoner.puuid);
+  }
+  matchIds = [].concat.apply([], matchIds);
+  for (let matchId of matchIds) {
+    console.log("For : ", matchId);
+    let matchObj = {};
+    //matchInfo
+    matchObj.matchInfo = [utils.generateMatchInfoDTO(matchId)];
+    //players
+    let playerList = [];
+    for (var x = 0; x < 10; x++) {
+      playerList.push(utils.generatePlayerDTO());
+    }
+    playerList[0].puuid = summoner.puuid;
+    matchObj.players = playerList;
+    //teams
+    let teams = [];
+    let arr = ["Red", "Blue"];
+    teamBool = casual.boolean;
+    for (var t = 0; t < 2; t++) {
+      teams.push(utils.generateTeamDTO(arr[t], teamBool));
+    }
+    matchObj.teams = teams;
+    //roundResults
+    matchObj.roundResults = [];
+    for (var r = 0; r < 12; r++) {
+      matchObj.roundResults.push(utils.generateRoundResultDTO(r));
+    }
+    //Push to DB
+    db.get("matches").push({ id: matchId, data: matchObj }).write();
+  }
+  res.send(
+    `Successfully generated ${n} summoners and ${matchIds.length} matches!`
+  );
+});
 
 app.listen(port, () =>
   console.log(`ValorSight Mock listening at http://localhost:${port}`)
