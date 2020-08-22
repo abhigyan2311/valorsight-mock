08be4b7b34cd94e21da7dca2972a78774d825c58
Abhigyan Singh <abhigyan2311@gmail.com>
2020-08-22 18:16:15 -0400

    commented generator

--- server.js
+++ server.js
@@ -115,62 +115,62 @@ app.get("/", (req, res) => {
 
 app.get("/status", statusMonitor.pageRoute);
 
-app.get("/init", (req, res) => {
-	db.defaults({ summoners: [], matches: [] }).write();
-	res.send("Successfully initialized DB!");
-});
+// app.get("/init", (req, res) => {
+// 	db.defaults({ summoners: [], matches: [] }).write();
+// 	res.send("Successfully initialized DB!");
+// });
 
-app.get("/generate", (req, res) => {
-	let matchIds = [];
-	let summonerIdsList = [];
-	for (var i = 0; i < n; i++) {
-		var summoner = {};
-		summoner.puuid = casual.uuid;
-		summonerIdsList.push(summoner.puuid);
-		summoner.gameName = casual.username;
-		summoner.tagLine = casual.integer((from = 1), (to = 1000));
-		summoner.activeShard = activeShards[Math.floor(Math.random() * activeShards.length)];
-		summoner.game = "val";
-		summoner.matchList = new Array(10).fill().map(() => casual.uuid);
-		matchIds.push(summoner.matchList);
-		db.get("summoners").push(summoner).write();
-		console.log("summoner saved: ", summoner.puuid);
-	}
-	let masterIdList = [];
-	for (let t of summonerIdsList) {
-		masterIdList.push(t);
-		masterIdList.push(t);
-	}
-	console.log(masterIdList);
-	matchIds = [].concat.apply([], matchIds);
-	for (let matchIndex in matchIds) {
-		let matchObj = {};
-		//matchInfo
-		matchObj.matchInfo = [utils.generateMatchInfoDTO(matchIds[matchIndex])];
-		//players
-		let playerList = [];
-		for (var x = 0; x < 10; x++) {
-			playerList.push(utils.generatePlayerDTO());
-		}
-		playerList[0].puuid = masterIdList[matchIndex];
-		matchObj.players = playerList;
-		//teams
-		let teams = [];
-		let arr = ["Red", "Blue"];
-		teamBool = casual.boolean;
-		for (var t = 0; t < 2; t++) {
-			teams.push(utils.generateTeamDTO(arr[t], teamBool));
-		}
-		matchObj.teams = teams;
-		//roundResults
-		matchObj.roundResults = [];
-		for (var r = 0; r < 12; r++) {
-			matchObj.roundResults.push(utils.generateRoundResultDTO(r, masterIdList[matchIndex]));
-		}
-		//Push to DB
-		db.get("matches").push({ id: matchIds[matchIndex], data: matchObj }).write();
-	}
-	res.send(`Successfully generated ${n} summoners and ${matchIds.length} matches!`);
-});
+// app.get("/generate", (req, res) => {
+// 	let matchIds = [];
+// 	let summonerIdsList = [];
+// 	for (var i = 0; i < n; i++) {
+// 		var summoner = {};
+// 		summoner.puuid = casual.uuid;
+// 		summonerIdsList.push(summoner.puuid);
+// 		summoner.gameName = casual.username;
+// 		summoner.tagLine = casual.integer((from = 1), (to = 1000));
+// 		summoner.activeShard = activeShards[Math.floor(Math.random() * activeShards.length)];
+// 		summoner.game = "val";
+// 		summoner.matchList = new Array(10).fill().map(() => casual.uuid);
+// 		matchIds.push(summoner.matchList);
+// 		db.get("summoners").push(summoner).write();
+// 		console.log("summoner saved: ", summoner.puuid);
+// 	}
+// 	let masterIdList = [];
+// 	for (let t of summonerIdsList) {
+// 		masterIdList.push(t);
+// 		masterIdList.push(t);
+// 	}
+// 	console.log(masterIdList);
+// 	matchIds = [].concat.apply([], matchIds);
+// 	for (let matchIndex in matchIds) {
+// 		let matchObj = {};
+// 		//matchInfo
+// 		matchObj.matchInfo = [utils.generateMatchInfoDTO(matchIds[matchIndex])];
+// 		//players
+// 		let playerList = [];
+// 		for (var x = 0; x < 10; x++) {
+// 			playerList.push(utils.generatePlayerDTO());
+// 		}
+// 		playerList[0].puuid = masterIdList[matchIndex];
+// 		matchObj.players = playerList;
+// 		//teams
+// 		let teams = [];
+// 		let arr = ["Red", "Blue"];
+// 		teamBool = casual.boolean;
+// 		for (var t = 0; t < 2; t++) {
+// 			teams.push(utils.generateTeamDTO(arr[t], teamBool));
+// 		}
+// 		matchObj.teams = teams;
+// 		//roundResults
+// 		matchObj.roundResults = [];
+// 		for (var r = 0; r < 12; r++) {
+// 			matchObj.roundResults.push(utils.generateRoundResultDTO(r, masterIdList[matchIndex]));
+// 		}
+// 		//Push to DB
+// 		db.get("matches").push({ id: matchIds[matchIndex], data: matchObj }).write();
+// 	}
+// 	res.send(`Successfully generated ${n} summoners and ${matchIds.length} matches!`);
+// });
 
 app.listen(port, () => console.log(`ValorSight Mock listening at http://localhost:${port}`));
